4aef6dc8df46aac0dd9e7af850c05949ab1a409f
segev955 <segev95@gmail.com>
2024-07-10 13:24:13 +0300

    work

--- OBD-II.py
+++ OBD-II.py
@@ -12,7 +12,6 @@ import threading
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import uuid
 
 cred = credentials.Certificate(
     "/home/segev/Project/OBDII-Data/car-driver-bc91f-firebase-adminsdk-xhkyn-214c09b623.json")
@@ -40,11 +39,12 @@ TEST1 = 0x50
 
 PID_REQUEST = 0x7DF
 PID_REPLY = 0x7E8
-
+#
+# user_name = input("Please enter your name(only first name): ")
+# car_type = input("please enter your car type: ")
 print(f"Hello {user_name}, Have a nice Drive!")
 
-drive_id = str(uuid.uuid4())  # Generate a unique ID for each drive
-outfile = open(f'{user_name}_{drive_id}.txt', 'a')
+outfile = open(f'{user_name}.txt', 'a')
 
 print('\n\rCAN Rx test')
 print('Bring up CAN0....')
@@ -75,9 +75,8 @@ drivers_ref = db.collection('Drivers').add({
     'cars_id': cars_id
 })
 drivers_id = drivers_ref[1].id
-
-def upload_drive_to_firestore(data, drive_id):
-    db.collection('Drives').document(drive_id).set(data)
+def upload_drive_to_firestore(data):
+    db.collection('Drives').add(data)
 
 
 def update_speed_limit():
@@ -231,7 +230,7 @@ except KeyboardInterrupt:
                 'speed_limit': speed_limitt,
                 'cars_id': cars_id,
                 'drivers_id': drivers_id
-            }, drive_id)
+            })
 
     # Clean up
     os.system("sudo /sbin/ip link set can0 down")
