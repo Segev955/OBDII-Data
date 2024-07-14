6b04f0239d1765d046fb76d9aad5f47149c2d910
SaliSharfman <salishar2@gmail.com>
2024-07-14 20:17:58 +0300

    add Obd class and update firebase communication

--- Listener.py
+++ Listener.py
@@ -1,83 +1,98 @@
-import os
-import time
-import subprocess
-import firebase_admin
-from firebase_admin import credentials, db
+from Obd_class import *
+import atexit
+import threading
+
+def on_exit():
+    obd_device.shutDown()
+    print("The program is exiting. Shutting down...")
 
 
-# Initialize Firebase Admin SDK
-while True:
+
+def entrylistener(event):
+    if event.data is not None:
+        print("Event path:", event.path)
+        print("Event data:", event.data)
+        if event.path == "/" and "userId" in event.data and "key" in event.data:
+            uid = event.data.get("userId", "Unknown")
+            key = event.data.get("key", "Nothing")
+
+            if obd_device.connect(uid,key):
+                db.reference(OBD_REFERENCE).child(obd_device.id).listen(drivelistener)
+
+
+
+def drivelistener(event):
+    if event.data is not None:
+        if event.path == "/":
+            status = event.data.get("status", "no_status")
+            alive = event.data.get("is_alive", False)
+            if not alive:
+                obd_device.setAlive()
+            print(f"OBD status: <{status}>")
+        else:
+            print("Event path:", event.path)
+            print("Event data:", event.data)
+
+            if event.path == "/status":
+                if isinstance(event.data, str):
+                    status = event.data
+                    if status == 'start':
+                        obd_device.startDriving()
+                    elif status == 'stop':
+                        obd_device.stopDriving()
+                    elif status == 'disconnected':
+                        obd_device.disconnect()
+                        db.reference(ENTRIES_REFERENCE).child(obd_device.id).listen(entrylistener)
+                    elif status == 'Shutting down':
+                        obd_device.shutDown()
+                    else:
+                        print(f"got unknown status: <{status}>")
+
+
+
+if __name__ == '__main__':
+    atexit.register(on_exit)
+    obd_device = Obd()
+
+    # Initialize Firebase Admin SDK
+    while True:
+        try:
+            cred = credentials.Certificate(f"{os.getcwd()}/car-driver-bc91f-firebase-adminsdk-xhkyn-214c09b623.json")
+            firebase_admin.initialize_app(cred, {
+                'databaseURL': 'https://car-driver-bc91f-default-rtdb.asia-southeast1.firebasedatabase.app/'
+            })
+            print("Firebase initialized successfully.")
+            break
+        except Exception as e:
+            print(f"Error initializing Firebase: {e}. Retrying in 5 seconds...")
+            obd_device.shutDown()
+            time.sleep(1)
+
+
+    # Schedule set_active to run periodically
+    def set_alive_periodically():
+        while True:
+            obd_device.setAlive()
+            time.sleep(10)  # Run this check every 10 seconds
+
+
+    # Run set_active in a separate thread
+    threading.Thread(target=set_alive_periodically, daemon=True).start()
+
+    # Get Available for connections
     try:
-        cred = credentials.Certificate("/home/segev/Project/OBDII-Data/car-driver-bc91f-firebase-adminsdk-xhkyn-214c09b623.json")
-        firebase_admin.initialize_app(cred, {
-            'databaseURL': 'https://car-driver-bc91f-default-rtdb.asia-southeast1.firebasedatabase.app/'
-        })
-        print("Firebase initialized successfully.")
-        break
+        success = False
+        while not success:
+            print("Attempting to set up Firebase listener...")
+            try:
+                db.reference(ENTRIES_REFERENCE).child(obd_device.id).listen(entrylistener)
+                obd_device.startUp()
+                success = True
+            except Exception as e:
+                print(f"Error setting up Firebase listener: {e}. Retrying in 5 seconds...")
+                time.sleep(5)
     except Exception as e:
-        print(f"Error initializing Firebase: {e}. Retrying in 5 seconds...")
-        time.sleep(1)
-
-def update_status(status):
-    db.reference('status').set(status)
-def run_data_collection_script(uid, car_type):
-    print(f"Start command received. Driver: {uid}, Car Type: {car_type}. Running data collection script...")
-    env = os.environ.copy()
-    env["DRIVER_UID"] = uid
-    env["CAR_TYPE"] = car_type
-
-    try:
-        subprocess.Popen(["python3", "/home/segev/Project/OBDII-Data/OBD_II.py"], env=env)
-        update_status("Started successfully")
-        print("Script started successfully.")
-    except Exception as e:
-        update_status(f"Error starting script: {e}")
-        print(f"Error running script: {e}")
-
-
-def stop_data_collection_script():
-    print("Stop command received. Sending SIGINT to data collection script...")
-    try:
-        subprocess.run(["pkill", "-SIGINT", "-f", "OBD_II.py"], check=True)
-        update_status("Stopped successfully")
-        print("Script stopped successfully.")
-    except subprocess.CalledProcessError as e:
-        update_status(f"Error stopping script: Noting to stop")
-        print(f"Error stopping script: {e}")
-
-def shutdown_raspberry():
-    print("Shutdown command received. Shutting down the Raspberry Pi...")
-    update_status("Shutting down")
-    subprocess.run(["sudo", "shutdown", "-h", "now"])
-
-
-
-
-def listener(event):
-    print("Event path:", event.path)
-    print("Event data:", event.data)
-
-    if event.path == "/":
-        if event.data.get("start"):
-            print("Start command detected")
-            uid = event.data.get("uid", "Unknown")
-            car_type = event.data.get("carType", "Unknown")
-            run_data_collection_script(uid, car_type)
-    elif event.path == "/stop" and event.data:
-        print("Stop command detected")
-        stop_data_collection_script()
-    elif event.path == "/shutdown" and event.data:
-        print("Shutdown command detected")
-        shutdown_raspberry()
-
-
-success = False
-while not success:
-    print("Attempting to set up Firebase listener...")
-    try:
-        db.reference('commands').listen(listener)
-        update_status("Ready..")
-        success = True
-    except Exception as e:
-        print(f"Error setting up Firebase listener: {e}. Retrying in 5 seconds...")
+        obd_device.shutDown()
+        print(f"Crashed with error {e}.")
         time.sleep(5)
+
---
+++ Obd_class.py
@@ -0,0 +1,145 @@
+import os
+import time
+import subprocess
+import firebase_admin
+import json
+import subprocess
+from firebase_admin import credentials, db
+
+
+ID = '4567'
+NAME = "fake4567"
+KEY = '4567'
+
+OBD_REFERENCE = 'Obd'
+ENTRIES_REFERENCE = 'ObdEntries'
+USERS_REFERENCE = 'Users'
+
+class Obd:
+    def __init__(self, id = ID,name = NAME, key = KEY):
+        self.id = id
+        self.name = name
+        self.key = key
+        self.status = ''
+        self.connected_uid = ''
+        self.is_alive = False
+        self.is_available = False
+
+    def setAlive(self):
+        self.is_alive =True
+        self.updateData()
+
+    def isConnected(self):
+        return self.connected_uid != ''
+
+    def updateStatus(self, status = 'available'):
+        self.status = status
+        self.updateData()
+
+    def updateUserStatus(self, status , uid ):
+        if uid is not None and uid !='':
+            print(f'setting status <{status}> to user <{uid}>')
+            db.reference(USERS_REFERENCE).child(uid).child('status').set(f'OBD {self.name}: {status}')
+
+
+    def connect(self, uid, key):
+        db.reference(ENTRIES_REFERENCE).child(self.id).delete()
+        if self.is_available and not self.isConnected():
+            print("checking key for connection")
+            if self.key == key:
+                self.connected_uid = uid
+                self.is_available = False
+                self.updateUserStatus(f'CONNECTED', self.connected_uid)
+                self.updateStatus("Connected")
+                print(f"User {uid} connected successfully!")
+                return True
+            else:
+                self.updateUserStatus(f"WRONG KEY", uid)
+                print(f"Got connection with wrong key")
+        print(f"Connection failed! {uid}")
+        return False
+
+    def disconnect(self):
+        if self.isConnected():
+            db.reference(USERS_REFERENCE).child(self.connected_uid).child('connected_obd').set('')
+            self.connected_uid = ''
+            print(f"User disconnected!")
+        self.startUp()
+
+
+    def startUp(self):
+        self.is_alive = True
+        self.is_available = True
+        print("Ready for connections...")
+        self.updateStatus("Ready")
+
+    def shutDown(self):
+        if self.isConnected():
+            db.reference(USERS_REFERENCE).child(self.connected_uid).child('connected_obd').set('')
+            self.connected_uid = ''
+            print(f"User disconnected!")
+        print("Shutdown command received. Shutting down the Raspberry Pi...")
+        self.is_alive = False
+        self.is_available = False
+        self.updateStatus("turned off")
+        try:
+            subprocess.run(["sudo", "shutdown", "-h", "now"])
+
+        except Exception as e:
+            print(f"Error, no OBD device connected: {e}.")
+
+
+    def updateData(self):
+        dict = {
+            'id': self.id,
+            'name': self.name,
+            'status': self.status,
+            'connected_uid': self.connected_uid,
+            'is_alive': self.is_alive,
+            'is_available': self.is_available,
+            'is_connected': self.isConnected()
+        }
+        db.reference(OBD_REFERENCE).child(self.id).set(dict)
+        if self.isConnected():
+            db.reference(USERS_REFERENCE).child(self.connected_uid).child('connected_obd').set(self.id)
+
+
+
+    def startDriving(self):
+        self.updateStatus("starting...")
+        uid = self.connected_uid
+        # Retrieve the car_type from the database
+        try:
+            car_type_snapshot = db.reference(USERS_REFERENCE).child(uid).child('carType').get()
+            if car_type_snapshot:
+                car_type = car_type_snapshot
+                print(f"Start command received. Driver: {uid}, Car Type: {car_type}. Running data collection script...")
+                env = os.environ.copy()
+                env["DRIVER_UID"] = uid
+                env["CAR_TYPE"] = car_type
+
+                try:
+                    subprocess.Popen(["python3", f"{os.getcwd()}/OBD_II.py"], env=env)
+                    self.updateStatus("driving")
+                    print("Script started successfully.")
+                except Exception as e:
+                    self.updateStatus(f"Error starting script: {e}")
+                    print(f"Error running script: {e}")
+            else:
+                print(f"Car type not found for user {uid}")
+        except Exception as e:
+            print(f"Error retrieving car type for user {uid}: {e}")
+
+    def stopDriving(self):
+        self.updateStatus("stopping...")
+        print("Stop command received. Sending SIGINT to data collection script...")
+        try:
+            subprocess.run(["pkill", "-SIGINT", "-f", "OBD_II.py"], check=True)
+            self.updateStatus("Stopped successfully")
+            print("Script stopped successfully.")
+        except subprocess.CalledProcessError as e:
+            self.updateStatus(f"Error stopping script: Nothing to stop")
+            print(f"Error stopping script (CalledProcessError): {e}")
+        except Exception as e:
+            self.updateStatus(f"Error stopping script: {e}")
+            print(f"Error stopping script (General Exception): {e}")
