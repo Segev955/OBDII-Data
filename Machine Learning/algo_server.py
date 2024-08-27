117a981711fc8e27a4c32ca6076cb97f5d0a8cf5
YasminCohen <103997973+YasminCohen@users.noreply.github.com>
2024-08-27 23:03:32 +0300

    Update algo_server.py

--- Machine Learning/algo_server.py
+++ Machine Learning/algo_server.py
@@ -7,21 +7,12 @@ from enum import Enum
 import firebase_admin
 from firebase_admin import credentials, db
 from keras import models
-from pre_process import pre_process
+from prediction_model import model_prediction
 
 REAL_TIME_REFERANCE = 'LiveData'
 OBD_REFERENCE       = 'Obd'
-TIMESTAMPS          = 100
 
-obd_dict = {} #{key: value} - key: obd_id, value: row_counter
-
-model_file  = 'LSTM_model.keras'
-try: 
-    model = models.load_model(model_file)
-except FileNotFoundError as e:
-    raise FileNotFoundError('Model not found - add the model.keras file to the dir.') from e
-
-
+obd_dict = {} 
 
 def get_live_data():
     while True:
@@ -43,37 +34,51 @@ def get_live_data():
                 print(f'Live drives: {obd_dict.keys()}')
             
             lock = threading.Lock()
-            with lock:
-                thread = threading.Thread(target=run_algorithm, args=(obd_id,))
-                thread.start()
+            # with lock:
+                # start_time = time.time()
+                # thread = threading.Thread(target=run_algorithm, args=(obd_id, start_time,))
+
+            #     thread.start()
+            start_time = time.time()
+            run_algorithm(obd_id, start_time) 
 
 
-        time.sleep(20)  # Run this check every 20 seconds
+        time.sleep(5) 
 
 
-def run_algorithm(obd_id):
+def run_algorithm(obd_id, start_time):
     data =[]
-    for _ in range(TIMESTAMPS):
+    first5rows = True
+    while True:
         row_index = obd_dict[obd_id]
+        print(f' \nrow index = {row_index} \n') #TODO delete, only for testing
         obd_ref = db.reference(f'{REAL_TIME_REFERANCE}/{obd_id}')
         obd_snapshot = obd_ref.get()
-        # print(list(obd_snapshot.keys())[0])
+        print(f'len of obd snap = {len(obd_snapshot)}')
+
         finished = False
 
+        new_data = []
         if isinstance(obd_snapshot, dict):
             if obd_snapshot and str(row_index) in list(obd_snapshot.keys()):
-                data.append(obd_snapshot[str(row_index)])
+
+                for i in range(row_index, len(obd_snapshot)): 
+                    new_data.append(obd_snapshot[str(i)]) 
+                data.extend(new_data)
             else:
                 finished = True
 
         elif isinstance(obd_snapshot, list):
             if obd_snapshot and len(obd_snapshot) > row_index:
-                data.append(obd_snapshot[row_index])
+
+                for i in range(row_index, len(obd_snapshot)): 
+                    new_data.append(obd_snapshot[i]) 
+                data.extend(new_data)
             else:
                 finished = True
 
         if finished:
-            #del obd_dict[obd_id]
+
             print(f'no more data from OBD {obd_id}')
             break
 
@@ -81,60 +86,44 @@ def run_algorithm(obd_id):
             print(f'Checked OBD {obd_id} row {row_index}: {data[-1]}')
 
 
-        ####################### APPLY ALGORITHM HERE ##################################
-
-        # data- a list of dictionaries, every dictionary is a second from the driving
-        # the list grows every iteration, the list is ordered
-        # data[0] is the first second from the driving
-        # data[-1] is the last second that never checked
-        # row_index is the index of the row, but it's better to use data[-1] because row_index not always starts at 0
-        # you should update driver_result to the UID of the driver that the algorithm found that he is the driver.
-
-        ## EXAMPLES
         print(f'Current Thread = {threading.current_thread()}')
 
         uids        = ['2W5Nq5aZ4cP9VA6zEWBbi7FicxE2', 'lT3ip6zL8gU34vuoONy5UTmWwPg1', 'vcAN0KURuBYtNhztFCJJR9y4EhR2']
         
 
-        if len(data) <= 5 :
-            input           = pre_process(data)
-            prediction      = (model.predict(input))[0][-1]
-            driver_result   = uids[np.argmax(prediction)]
-            
-            print(f'prediction= {prediction}' )
+        obd_dict[obd_id] = len(obd_snapshot)
+        
+        if first5rows and len(data)>=5:
+            first5rows = False
+            data = data[5:]
 
-            print(f'driver result = \n {driver_result}')
+        print('len data = ', len(data))
 
-            # print(data[-1]['datetime']) # should print the last date and time from the data
-            # if 'rpm' in list(data[-1].keys()):
-            #     print(data[-1]['rpm'])  # should print the last rpm from the data
-            
-            # for i in range(len(data)):
-            #     print(data[i]['speed'])  # should print all the speed data from the start until now
-            
+        prediction, data = model_prediction(uids, data)
+        print(f'prediction= {prediction}')
 
-            # driver_result = 'lT3ip6zL8gU34vuoONy5UTmWwPg1' # end of the iteration, save the current result
-            obd_dict[obd_id] += 1
-            db.reference(OBD_REFERENCE).child(obd_id).child('last_driver').set(driver_result)
-            #time.sleep(1)
+        max_speed = max(item['speed'] for item in data)
+        print(f'max speed = {max_speed}')
+        if prediction and (time.time() - start_time >= 20*60 or max_speed > 30):
+            db.reference(OBD_REFERENCE).child(obd_id).child('last_driver').set(prediction)
+            break
+        else:
+            prediction = prediction if prediction != 'STOLEN' else 'UNKOWN DRIVER'
+            db.reference(OBD_REFERENCE).child(obd_id).child('last_driver').set(prediction)
 
-        if max(prediction) > 0.8: #if prediction certainty is greater than 80%
-            return #TODO do we need to return a value? e.g. driver_result
-        #TODO change while True to a for loop after which car reported stolen (ideal), or add a "forth" driver to the model that will be the thief.
-    
-    db.reference(OBD_REFERENCE).child(obd_id).child('last_driver').set('STOLEN')
+        #time.sleep(1)
 
 
 def run_algo_server():
     print("Start listening for new drives...")
-    # threading.Thread(target=get_live_data, daemon=True).start() #TODO
+
     get_live_data()
 
 if __name__ == '__main__':
-    # Initialize Firebase Admin SDK
+
     while True:
         try:
-            cred = credentials.Certificate(f"{os.getcwd()}/car-driver.json")
+            cred = credentials.Certificate(f"{os.getcwd()}/car-driver-bc91f-firebase-adminsdk-xhkyn-214c09b623.json")
             firebase_admin.initialize_app(cred, {
                 'databaseURL': 'https://car-driver-bc91f-default-rtdb.asia-southeast1.firebasedatabase.app/'
             })
